f83048a0e26354d54c0e779075cf0a96a09b3f93
lmarchisio <51975294+lmarchisio@users.noreply.github.com>
2019-07-22 10:33:51 -0700

    Create webhooks_create.py

---
+++ webhooks_create.py
@@ -0,0 +1,31 @@
+import smartsheet
+import logging
+
+# Set API access token
+access_token = None
+
+# initialize client
+ss_client = smartsheet.Smartsheet(access_token)
+# make sure we don't miss any errors
+ss_client.errors_as_exceptions(True)
+# log all calls
+logging.basicConfig(filename='rwsheet.log', level=logging.INFO)
+
+# TODO enter Smartsheet API Access Token
+# change callbackUrl to function trigger
+
+print('To create webhook enter sheet ID')
+this_sheet = input()
+
+# create webhook
+Webhook = ss_client.Webhooks.create_webhook(
+    smartsheet.models.Webhook({
+        'name': 'Webhook #4',
+        'callbackUrl': 'https://google-cloud-function-trigger',
+        'scope': 'sheet',
+        'scopeObjectId': this_sheet,
+        'events': ['*.*'],
+        'version': 1}))
+
+# Finish it
+print('Done')
