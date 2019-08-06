c1e7679c58d8d0ffee7c93ddcb0d8a48ba5db4f4
lmarchisio <51975294+lmarchisio@users.noreply.github.com>
2019-08-06 09:25:24 -0700

    Update webhooks_list.py
    creates an easier to read list

--- webhooks_list.py
+++ webhooks_list.py
@@ -1,22 +1,30 @@
 import smartsheet
 import logging
+import json
+import pprint
+
+# list webhooks
 
 # Set API access token
 access_token = None
 
 # initialize client
-ss_client = smartsheet.Smartsheet(access_token)
+smart = smartsheet.Smartsheet(access_token)
 # make sure we don't miss any errors
-ss_client.errors_as_exceptions(True)
+smart.errors_as_exceptions(True)
 # log all calls
 logging.basicConfig(filename='rwsheet.log', level=logging.INFO)
 
-# TODO add Smartsheet API access token
+IndexResult = smart.Webhooks.list_webhooks(
+    page_size=100,
+    page=1,
+    include_all=False)
 
-# list webhooks
-IndexResult = ss_client.Webhooks.list_webhooks(
-    page_size = 100,
-    page = 1,
-    include_all = False)
+a = json.loads(str(IndexResult))
 
-print(IndexResult)
+pprint.pprint(a.get('data', 0))
+
+# uncomment below to also print 'results' data
+# pprint.pprint(a)
+
+
