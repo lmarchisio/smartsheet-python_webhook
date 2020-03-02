f23cbf0de5580f8975ca58e094d2d1bd67683e6d
lmarchisio <51975294+lmarchisio@users.noreply.github.com>
2020-03-02 11:40:29 -0800

    Create main2.py
    Alternate main for updating with info from the sheet summary

---
+++ main2.py
@@ -0,0 +1,155 @@
+#import smartsheet
+#import json
+
+#access_token = None
+#column_map = {}
+#smart = smartsheet.Smartsheet(access_token)
+
+#def get_cell(row, column_name):
+#    column_id = column_map[column_name]
+#    return row.get_column(column_id)
+    
+#def make_pm(source_row, pm):
+#    new_cell = smartsheet.models.Cell()
+#    new_cell.column_id = column_map['PM']
+#    new_cell.value = pm
+    
+#    new_row = smartsheet.models.Row()
+#    new_row.id = source_row.id
+#    new_row.cells.append(new_cell)
+    
+#    return new_row
+
+#def update_pm(request):
+#    """Responds to any HTTP request.
+#    Args:
+#        request (flask.Request): HTTP request object.
+#    Returns:
+#        The response text or any set of values that can be turned into a
+#        Response object using
+#        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+#    """
+#    request_json = request.get_json()
+#    # if this is a challenge, return verification
+#    if request.args and 'challenge' in request.args:
+#        return None
+#    elif request_json and 'challenge' in request_json:
+#        return json.dumps({
+#            "smartsheetHookResponse": request_json['challenge']
+#        })
+#    elif request_json and 'scopeObjectId' in request_json:
+#        sheetid = request_json['scopeObjectId']
+#        sheet = smart.Sheets.get_sheet(sheetid)
+        
+#        for column in sheet.columns:
+#            column_map[column.title] = column.id
+        
+#        row_map = {}
+#        for row in sheet.rows:
+#            row_map[row.row_number] = row
+        
+#        pm_cell = get_cell(row_map[1], 'PM')
+
+#        rowsToUpdate = [row for row in sheet.rows if get_cell(row, "PM").display_value is None]
+
+#        if rowsToUpdate != []:
+#            pm_rows = []
+#            for row in rowsToUpdate:
+#                write_row = make_pm(row, pm_cell.value)
+#                pm_rows.append(write_row)
+
+#            result = smart.Sheets.update_rows(sheetid, pm_rows)
+
+            
+import smartsheet
+import json
+
+access_token = None
+column_map = {}
+status_columns = {}
+smart = smartsheet.Smartsheet(access_token)
+
+def get_cell(row, column_name):
+    column_id = column_map[column_name]
+    return row.get_column(column_id)
+
+def get_status_cell(row, column_name):
+    column_id = status_columns[column_name]
+    return row.get_column(column_id)
+
+def find_pm(sheetid):
+    response = smart.Sheets.get_sheet_summary_fields(
+        sheetid
+    )
+
+    a = json.loads(str(response))
+
+    if 'data' in a:
+        b = a['data']
+        for thing in b:
+            if thing['title'] == 'PM':
+                if 'objectValue' in thing:
+                    if 'email' in thing['objectValue']:
+                        return((thing['objectValue'])['email'])
+    else:
+        return None
+
+def make_pm(source_row, pm):
+    new_cell = smartsheet.models.Cell()
+    new_cell.column_id = column_map['PM']
+    new_cell.value = pm
+    
+    new_row = smartsheet.models.Row()
+    new_row.id = source_row.id
+    new_row.cells.append(new_cell)
+    
+    return new_row
+
+def update_pm(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
+    request_json = request.get_json()
+    # if this is a challenge, return verification
+    if request.args and 'challenge' in request.args:
+        return None
+    elif request_json and 'challenge' in request_json:
+        return json.dumps({
+            "smartsheetHookResponse": request_json['challenge']
+        })
+    elif request_json and 'scopeObjectId' in request_json:
+        sheetid = request_json['scopeObjectId']
+        sheet = smart.Sheets.get_sheet(sheetid)
+        the_pm = find_pm(sheetid)
+#        project_status = smart.Sheets.get_sheet(2762627773425540)
+        
+#        for column in project_status.columns:
+#            status_columns[column.title] = column.id
+           
+        for column in sheet.columns:
+            column_map[column.title] = column.id
+
+#        for row in project_status.rows:
+#            number = get_status_cell(row, 'Project #')
+#            name = get_status_cell(row, 'Project Name')
+#            test = number.display_value + " " + name.display_value
+#            if test == sheet.name:
+#                pm_cell = get_status_cell(row, 'Project Manager')
+
+        rowsToUpdate = [row for row in sheet.rows if get_cell(row, "PM").display_value is None]
+
+        if rowsToUpdate != []:
+            pm_rows = []
+            for row in rowsToUpdate:
+                write_row = make_pm(row, the_pm)
+#                write_row = make_pm(row, pm_cell.value)
+                pm_rows.append(write_row)
+
+            result = smart.Sheets.update_rows(sheetid, pm_rows)
+
+
